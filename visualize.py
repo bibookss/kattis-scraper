c782c2012dcad4c4cfdbcd9ef694c11297e3041c
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-07-12 12:54:01 +0800

    feat: language used chart

--- visualize.py
+++ visualize.py
@@ -10,7 +10,16 @@ def problem_difficulty(user):
 def ave_tries_per_problem(user):
 	pass
 
-def most_common_language_used(user):
+def language_used(user):
+    	df = pd.DataFrame(user.submissions)
+
+    	language_counts = df['language'].value_counts()
+
+    	plt.bar(language_counts.index, language_counts.values)
+    	plt.xlabel('Programming Language')
+    	plt.ylabel('Frequency')
+    	plt.title('Language Usage')
+    	plt.show()
+
+def most_successful_language(user):
 	pass
-
-
