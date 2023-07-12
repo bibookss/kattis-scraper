131af6128d03bdb90a0eb084cf5c7cdcb0c161c3
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-07-12 12:55:00 +0800

    feat: most successful language chart

--- visualize.py
+++ visualize.py
@@ -21,5 +21,19 @@ def language_used(user):
     	plt.title('Language Usage')
     	plt.show()
 
-def most_successful_language(user):
-	pass
+def most_succesful_language(user):
+    df = pd.DataFrame(user.submissions)
+    
+    accepted_df = df[df['status'] == 'Accepted']    
+    not_accepted_df = df[df['status'] != 'Accepted']
+    
+    accepted_counts = accepted_df['language'].value_counts()
+    not_accepted_counts = not_accepted_df['language'].value_counts()
+    
+    normalized_counts = accepted_counts / (accepted_counts + not_accepted_counts)
+    
+    plt.bar(normalized_counts.index, normalized_counts.values * 100)
+    plt.xlabel('Programming Language')
+    plt.ylabel('Acceptance Rate in Percentage')
+    plt.title('Most Successful Language')
+    plt.show()
